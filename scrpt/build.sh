e60ac0340c0ba2aa69eada146d8c735ec15f2a3e
goldsteink <kevin@sendence.com>
2017-03-07 17:04:55 -0500

    better builds

--- scrpt/build.sh
+++ scrpt/build.sh
@@ -12,66 +12,112 @@ C2P_PONY=/home/kgoldstein/dev/c++/msc2p4/src
 PROJ=/home/kgoldstein/dev/c++/msc2p4
 PRJSRC=$PROJ/src
 
+
+# arg1: exec value
+# arg2: should terminate
+# arg3: Text
+function printAndExit()
+{
+    if [ $# -lt 3 ] ; then
+	echoRed "Not enough arguments for the function"
+    fi
+
+    if [ $1 -gt 0 ] ; then
+	echoRed "Unable to build: $3" >> $BUILD_LOG 2>&1
+	echoRed "Unable to build: $3"
+	if [ $2 -gt 0 ] ; then
+	    exit 1
+	fi
+    else
+	echoYellow "$3 Finished" >> $BUILD_LOG 2>&1
+	echoYellow "$3 Finished"
+    fi
+}
+
+
+
+
+
 if [ ! -d $build_dir ] ; then
     mkdir -p $build_dir
 fi
 
-eghoGreen "Building SWIG..." > $BUILD_LOG 2>&1
-echoBlue "Building SWIG..."
-swig -python -external-runtime src/swigpyrun.h >> $BUILD_LOG 2>&1
-swig -shadow -cppext cpp -c++ -python src/Base.i >> $BUILD_LOG 2>&1
+if [ "$1" == "" ] || [ "$1" == "swig" ] ; then
+    echoBlue -n "Building SWIG..." > $BUILD_LOG 2>&1
+    echoBlue "Building SWIG..."
+
+    swig -python -external-runtime src/swigpyrun.h >> $BUILD_LOG 2>&1
+    printAndExit $? 1 "SWIG external runtime"
+
+    swig -shadow -cppext cpp -c++ -python src/Base.i >> $BUILD_LOG 2>&1
+    printAndExit $? 1 "SWIG shadow classes"
+fi
+
+
+if [ "$1" == "" ] || [ "$1" == "cpp" ] ; then
+    echoBlue "Building C++ Python Module... ">> $BUILD_LOG 2>&1
+    echoBlue "Building C++ Python Module..."
+    debugargs="-g3 -ggdb -O0"
+    
+    pushd src>> /dev/null 2>&1
+    c++ -fPIC -fpermissive $debugargs -march=native -mcx16 -fuse-ld=gold \
+	-shared -lpthread -ldl -lutil -lm -lpython2.7 -I/usr/include/python2.7 \
+	-I"." \
+	-I$WALL_INC_DIR -L$WALL_LIB_DIR -l$WALL_LIB_NAME \
+	-DDEBUG \
+	*.cpp -o _MSPY.so >> $BUILD_LOG 2>&1
+    printAndExit $? 1 "C++ MSPY library"
+
+    c++ -fPIC -fpermissive $debugargs -march=native -mcx16 -fuse-ld=gold \
+	-shared -lpthread -ldl -lutil -lm -lpython2.7 -I/usr/include/python2.7 \
+	-I"." \
+	-I$WALL_INC_DIR -L$WALL_LIB_DIR -l$WALL_LIB_NAME \
+	-DDEBUG \
+	-c market-spread-cpp.cpp >> $BUILD_LOG 2>&1
+    printAndExit $? 1 "C++ MSCPP Object"
+    popd>>/dev/null 2>&1
+fi
 
 
 pony_obj=src/src.o
-if [ -e $pony_obj ] ; then
-    echoRed "cleaning old object file: $pony_obj" >> $BUILD_LOG 2>&1
-    rm $pony_obj
-fi
-if [ -e src/msc2p4.o ] ; then
-    echoRed "cleaning old object file: src/msc2p4.o" >> $BUILD_LOG 2>&1
-    rm src/msc2p4.o
-fi
+if [ "$1" == "" ] || [ "$1" == "pony" ] ; then
+    echoBlue "Building pony" >> $BUILD_LOG 2>&1
+    echoBlue "Building pony"
 
-
-echoGreen "Building pony" >> $BUILD_LOG 2>&1
-echoGreen "Building pony"
-ponyc --path=$WALL_HOME:$WALL_LIB_DIR:$CPP_PONY:$C2P_LIB --output=src ./src >> $BUILD_LOG 2>&1
-if [ -e $pony_obj ] ; then
-    mv $pony_obj src/msc2p4.o
-fi
-
-
-echoGreen Building Python Module... >> $BUILD_LOG 2>&1
-pushd src
-c++ -fPIC -fpermissive -O0 -march=native -mcx16 -fuse-ld=gold \
--shared -lpthread -ldl -lutil -lm -lpython2.7 -I/usr/include/python2.7 \
--I$WALL_INC_DIR -L$WALL_LIB_DIR -l$WALL_LIB_NAME \
-*.cpp -o _MSPY.so >> $BUILD_LOG 2>&1
-if [ $? -gt 0 ] ; then
-    echoRed "Failed!" >> $BUILD_LOG 2>&1
-    echoRed "FAILED!"
-    exit 1
+    if [ -e $pony_obj ] ; then
+	echoRed "cleaning old object file: $pony_obj" >> $BUILD_LOG 2>&1
+	rm $pony_obj
+    fi
+    if [ -e src/msc2p4.o ] ; then
+	echoRed "cleaning old object file: src/msc2p4.o" >> $BUILD_LOG 2>&1
+	rm src/msc2p4.o
+    fi
+    
+    ponyc --path=$WALL_HOME:$WALL_LIB_DIR:$CPP_PONY:$C2P_LIB --output=src ./src >> $BUILD_LOG 2>&1
+    if [ -e $pony_obj ] ; then
+	printAndExit 0 1 "PONY compilation successfull"
+	mv $pony_obj src/msc2p4.o
+    else
+	printAndExit 1 1 "PONY compilation failed"
+    fi
 fi
 
 
-echoGreen "Linking overall project..." >> $BUILD_LOG 2>&1
-c++ -o $PRJSRC/msc2p4 -O0 -g -march=native -mcx16 -fuse-ld=gold $PRJSRC/*.o \
--L$WALL_HOME -Wl,-rpath,$WALL_HOME \
--L$WALL_API_DIR -Wl,-rpath,$WALL_API_DIR \
--L$PRJSRC -Wl,-rpath,$PRJSRC \
--L/usr/local/lib/pony/0.9.0-e405a8d/bin/../lib \
--Wl,-rpath,/usr/local/lib/pony/0.9.0-e405a8d/bin/../lib \
--L/usr/local/lib/pony/0.9.0-e405a8d/bin/../packages \
--Wl,-rpath,/usr/local/lib/pony/0.9.0-e405a8d/bin/../packages \
--L/usr/local/lib -Wl,-rpath,/usr/local/lib -Wl,--start-group \
--lwallaroo -lstdc++ -lrt -Wl,--end-group -lponyrt -lpthread -ldl -lm -lpython2.7 >> $BUILD_LOG 2>&1
-if [ $? -gt 0 ] ; then
-    echoRed "FAILED TO BUILD!"
-    echoRed "FAILED TO BUILD!" >> $BUILD_LOG
-else
-    echoBlue "Finished!"
-    echoBlue "Finished!" >> $BUILD_LOG
-
+pushd ./src >> /dev/null 2>&1
+if [ "$1" == "" ] || [ "$1" == "link" ] ; then
+    echoBlue "Pony/C++/Swig linking..." >> $BUILD_LOG 2>&1
+    echoBlue "Pony/C++/Swig linking..."
+    c++ -o $PRJSRC/msc2p4 -O0 -g -march=native -mcx16 -fuse-ld=gold $PRJSRC/*.o \
+	-L$WALL_HOME -Wl,-rpath,$WALL_HOME \
+	-L$WALL_API_DIR -Wl,-rpath,$WALL_API_DIR \
+	-L$PRJSRC -Wl,-rpath,$PRJSRC \
+	-L/usr/local/lib/pony/0.9.0-e405a8d/bin/../lib \
+	-Wl,-rpath,/usr/local/lib/pony/0.9.0-e405a8d/bin/../lib \
+	-L/usr/local/lib/pony/0.9.0-e405a8d/bin/../packages \
+	-Wl,-rpath,/usr/local/lib/pony/0.9.0-e405a8d/bin/../packages \
+	-L/usr/local/lib -Wl,-rpath,/usr/local/lib -Wl,--start-group \
+	-lwallaroo -lstdc++ -lrt -Wl,--end-group -lponyrt -lpthread -ldl -lm -lpython2.7 \
+	-l:_MSPY.so >> $BUILD_LOG 2>&1
+    printAndExit $? 1 "Pony/C++/Swig linking"
 fi
-
-popd
+popd >> /dev/null 2>&1
